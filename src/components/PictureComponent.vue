34f44bcf123cbc478f5c70011bb2e905e1ee7dfe
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:28:02 -0600

    Generated the component to pictures

---
+++ src/components/PictureComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card">
+        <div class="card-content has-text-centered">
+            <h2 class="subtitle is-2">Fotos</h2>
+            <carousel>
+                <slide>
+                    <img class="image" src="../assets/1.jpg" alt="">
+                </slide>
+                <slide>
+                    <img class="image" src="../assets/2.jpg" alt="">
+                </slide>
+                <slide>
+                    <img class="image" src="../assets/3.jpg" alt="">
+                </slide>
+                <slide>
+                    <img class="image" src="../assets/4.jpg" alt="">
+                </slide>
+                <slide>
+                    <img class="image" src="../assets/5.jpg" alt="">
+                </slide>
+                <slide>
+                    <img class="image" src="../assets/6.jpg" alt="">
+                </slide>
+            </carousel>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PictureComponent',
+    data() {
+        return {
+            img: [1, 2, 3, 4, 5, 6]
+        }
+    }
+}
+</script>
