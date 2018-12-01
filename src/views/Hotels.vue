f3497db288c81295e90d483aea9744df6764210c
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:26:55 -0600

    Created the view for hotels

---
+++ src/views/Hotels.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <br>
+        <h2 class="title is-2 has-text-centered">Hoteles</h2>
+        <div class="card">
+            <div class="card-content has-text-centered">
+                <div class="column is-3">
+                    <img src="../assets/imagen8.jpg">
+                </div>
+                <div class="column is-9">
+                    <p><b>Hotel La Luz</b></p>
+                    <p><b>Precio: </b>$250 la noche.</p>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-content has-text-centered">
+                <div class="column is-3">
+                    <img src="../assets/images6.jpg">
+                </div>
+                <div class="column is-9">
+                    <p><b>Hotel La Posada</b></p>
+                    <p><b>Precio: </b>$300 la noche.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import CardHotel from '@/components/hotels/CardHotel.vue'
+
+export default {
+    name: 'Hotels',
+    components: {
+        CardHotel
+    }
+}
+</script>
