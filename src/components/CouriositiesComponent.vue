4de9bedb4c3757b2120e29eb3ae4201ec331ccc0
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:27:22 -0600

    Generated component to render the couriosity data

---
+++ src/components/CouriositiesComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="card">
+        <div class="card-content has-text-centered">
+            <h2 class="subtitle is-2">Curiosidades</h2>
+            <img src="../assets/images14.jpg" alt="">
+        </div>
+    </div>
+</template>
