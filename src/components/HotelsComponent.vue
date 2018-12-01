e9c859eec5fb675af417406ad9b3b46c07980d3a
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:27:50 -0600

    Generated component to hotels

---
+++ src/components/HotelsComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="card">
+        <div class="card-content has-text-centered">
+            <h2 class="subtitle is-2">Donde hospedarte</h2>
+            <img src="../assets/images6.jpg" alt="">
+        </div>
+    </div>
+</template>
