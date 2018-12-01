e369e2558cc22c8c550dece931407c4f16e83533
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:29:27 -0600

    Added some structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar is-link">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <h3 class="subtitle is-3 has-text-white">
+            Go-2SanFe
+          </h3>
+        </router-link>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
@@ -13,19 +18,7 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
