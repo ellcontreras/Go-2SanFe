476825ba8d4b86edb3004e4194b3b0a3f3ced4e2
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:28:32 -0600

    Extra components

---
+++ src/components/hotels/CardHotel.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+        <div class="card-content">
+            <img :src="img">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardHotel',
+    props: {
+        img: String
+    }
+}
+</script>
