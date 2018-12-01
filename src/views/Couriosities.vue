374cfec5fc61e188804bb0724fdc163393c2c927
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:26:40 -0600

    Created the couriosities view

---
+++ src/views/Couriosities.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div v-for="(c, k) of couriosities" :key="k">
+            <p><b>{{ c.title }}</b></p>
+            <p>{{ c.content }}</p>
+            <br>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Couriosities',
+    data() {
+        return {
+            couriosities: [
+                {
+                    title: 'En San Felipe vivió Miguel Hidalgo',
+                    content: `Miguel Hidalgo durante un tiempo fue Sr. Cura de San Felipe
+                    y vivió en la calle Hidalgo #121`
+                },
+                {
+                    title: 'La Feria de San Miguel',
+                    content: `En el mes de Septiembre se celebra la feria de San Miguel Arcangel
+                    desde el 21 al 29 del mes`
+                },
+                {
+                    title: 'El Indio',
+                    content: `El indio es un monumento que representa ...`
+                }
+            ]
+        }
+    }
+}
+</script>
