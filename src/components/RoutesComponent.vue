81c4728050ef8f988b89c52b115a0a22509c09c4
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:28:20 -0600

    Generated the component to routes

---
+++ src/components/RoutesComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <div class="card-content has-text-centered">
+            <h2 class="subtitle is-2">
+                Rutas
+            </h2>
+            <img class="image" src="../assets/routes.png" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RoutesComponent'
+}
+</script>
+
+<style scoped>
+    img.image {
+        width: 100%;
+        height: 25em;
+    }
+</style>
