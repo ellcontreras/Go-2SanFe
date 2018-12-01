baf01160fbd08691c1c5b75f7c6163aba8d18afe
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:26:27 -0600

    Created the food view

---
+++ src/views/Food.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <br>
+        <h2 class="title is-2 has-text-centered">Comida</h2>
+        <div class="card">
+            <div class="card-content has-text-centered">
+                <div class="columns">
+                    <div class="column is-3">
+                        <img src="../assets/imagen11.jpg">
+                    </div>
+                    <div class="column is-9">
+                        <p><b>Gorditas Doña Meche</b></p>
+                        <p><b>Precio: </b>$10 por gorda.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-content has-text-centered">
+                <div class="column is-3">
+                    <img src="../assets/imagen12.jpg">
+                </div>
+                <div class="column is-9">
+                    <p><b>Enchiladas Petra</b></p>
+                    <p><b>Precio: </b>$30 la ordén.</p>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-content has-text-centered">
+                <div class="column is-3">
+                    <img src="../assets/images13.jpg">
+                </div>
+                <div class="column is-9">
+                    <p><b>Quesadillas el charo</b></p>
+                    <p><b>Precio: </b>$40 por quesadilla.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
