5e00caf4cda8fada8d7280c087233cb8eb31b908
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:27:35 -0600

    Generated the component for food

---
+++ src/components/FoodComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="card">
+        <div class="card-content has-text-centered">
+            <h2 class="subtitle is-2">Comida</h2>
+            <img src="../assets/imagen11.jpg" alt="">
+        </div>
+    </div>
+</template>
