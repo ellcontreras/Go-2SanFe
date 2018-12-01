76f48092ef57a15fbc617c01d8a22831c1dacd92
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-12-01 14:30:10 -0600

    imported some components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <router-link to="/routes">
+      <routes-component></routes-component>
+    </router-link>
+    <br>
+    <picture-component></picture-component>
+    <br>
+    <router-link to="/hotels">
+      <hotels-component></hotels-component>
+    </router-link>
+    <br>
+    <router-link to="/food">
+      <food-component></food-component>
+    </router-link>
+    <br>
+    <router-link to="/couriosities">
+      <curiosities-component></curiosities-component>
+    </router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RoutesComponent from '@/components/RoutesComponent.vue'
+import PictureComponent from '@/components/PictureComponent.vue'
+import HotelsComponent from '@/components/HotelsComponent.vue'
+import FoodComponent from '@/components/FoodComponent.vue'
+import CuriositiesComponent from '@/components/CouriositiesComponent.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    RoutesComponent,
+    PictureComponent,
+    HotelsComponent,
+    FoodComponent,
+    CuriositiesComponent
   }
 }
 </script>
